aaf279d7161853a611008b05422aa7cb24f0622c
Ali Taghva <taghvali99@gmail.com>
2020-12-17 21:19:44 -0500

    add random order for the one who starts the fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -72,55 +72,72 @@ var fightOrSkip = function () {
 }
 
 var fight = function (enemy) {
-  console.log(enemy);
+  // keep track of who goes first
+  var isPlayerTurn = true;
+
+  // randomly change turn order
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
 
   //repeat and execute as long as the enemy-robot is alive
   while (enemy.health > 0 &&  playerInfo.health > 0) {
-    // ask player if they'd like to fight or skip using fightOrSkip function
-    var isSkipping = fightOrSkip();
 
-    if (isSkipping) {
+    if (isPlayerTurn) {
+      // ask player if they'd like txo fight or skip using fightOrSkip function
+      var isSkipping = fightOrSkip();
+
+      if (isSkipping) {
       // if true, leave fight by breaking loop
       break;
-    }
+      }
     
-    // generate random damage value based on player's attack power
-    var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
     
-    // remove enemy's health by subtracting the amount set in the damage variable
-    enemy.health = Math.max(0, enemy.health - damage);
-    console.log(
-      `${playerInfo.name} attacked ${enemy.name}. ${enemy.name} now has ${enemy.health} health remaining.`
-    );
-  
+      // generate random damage value based on player's attack power
+      var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+      
+      // remove enemy's health by subtracting the amount set in the damage variable
+      enemy.health = Math.max(0, enemy.health - damage);
+      console.log(
+        `${playerInfo.name} attacked ${enemy.name}. ${enemy.name} now has ${enemy.health} health remaining.`
+      );
+    
 
-    // check enemy's health
-    if (enemy.health <= 0) {
-      window.alert(`${enemy.name} has died!`);
-      break;
-    } else {
-      window.alert(`${enemy.name} still has ${enemy.health} health left.`);
-    }
-  
-    
-    // generate random damage value based on enemy's attack power
-    var damage = randomNumber(enemy.attack - 3, enemy.attack);
-    
-    // remove player's health by subtracting the amount set in the damage variable
-    playerInfo.health = Math.max(0, playerInfo.health - damage);
-    console.log(
-      `${enemy.name} attacked ${playerInfo.name} . ${playerInfo.name} now has ${playerInfo.health} health remaining.`
-    );
-  
+      // check enemy's health
+      if (enemy.health <= 0) {
+        window.alert(`${enemy.name} has died!`);
 
-    // check player's health
-    if (playerInfo.health <= 0) {
-      window.alert(`${playerInfo.name} has died!`);
-      break;
+        // award player money for winning
+        playerInfo.money += 20;
+        break;
+      } else {
+        window.alert(`${enemy.name} still has ${enemy.health} health left.`);
+      }
     } else {
-      window.alert(`${playerInfo.name} still has ${playerInfo.health} health left.`);
-    }
+      // generate random damage value based on enemy's attack power
+      var damage = randomNumber(enemy.attack - 3, enemy.attack);
+      
+      // remove player's health by subtracting the amount set in the damage variable
+      playerInfo.health = Math.max(0, playerInfo.health - damage);
+      console.log(
+        `${enemy.name} attacked ${playerInfo.name} . ${playerInfo.name} now has ${playerInfo.health} health remaining.`
+      );
+    
+
+      // check player's health
+      if (playerInfo.health <= 0) {
+        window.alert(`${playerInfo.name} has died!`);
+        break;
+      } else {
+        window.alert(`${playerInfo.name} still has ${playerInfo.health} health left.`);
+      }
+    } 
+
+    // switch turn order for next round
+    isPlayerTurn = !isPlayerTurn;
+
   }
+
 };
 
 // function to end the game
@@ -207,7 +224,7 @@ var startGame = function () {
       window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
      
       // pick new enemy to fight based on the index of the enemyName array
-      var pickedEnemyObject = enemyInfo[i];
+      var pickedEnemyObject = enemyInfo[Math.floor( Math.random() * enemyInfo.length )];
   
       // reset enemyHealth before starting new fight
       pickedEnemyObject.health = randomNumber(40, 60);
